d573e0f05ff16e7fa67dca5752f222030c8c683c
Mckay <mckaywrigley@gmail.com>
2019-04-01 21:03:11 -0700

    wires up redux

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends Component {
+const Header = () => {
+    return (
+        <h2>Header</h2>
+    );
+}
+
+const Dashboard = () => {
+    return (
+        <h2>Dashboard</h2>
+    );
+}
+
+const SurveyNew = () => {
+    return (
+        <h2>SurveyNew</h2>
+    );
+}
+
+const Landing = () => {
+    return (
+        <h2>Landing</h2>
+    );
+}
+
+class App extends React.Component {
     render() {
         return (
             <div>
-                App
+                <BrowserRouter>
+                    <Route path="/" component={Landing} />
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/survey/new" component={SurveyNew} />
+                    <Route path="/header" component={Header} />
+                </BrowserRouter>
             </div>
         )
     }
